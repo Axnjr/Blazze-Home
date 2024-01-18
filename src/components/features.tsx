85c2c09eef83b2176c1002bc6035a1ed086e7e3a
Yakshit chhipa <yakshitchhipa@gamil.com>
2024-01-18 20:32:31 +0530

    style fix: font weight

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -3,11 +3,11 @@ import DogeSmile from "/doge.webp"
 
 const features = [
     { name: "Routing", description: "Keep the logic separated without any setup. Routes are based on the directory structure." },
+    { name: "Powered by SWC", description: "Super speed transpilation via the power of Rust." },
     { name: "Request Caching", description: "Blazze handles caching automatically to optimize large computations." },
     { name: "TypeScript", description: "No configurations and environment setup, just get started directly" },
-    { name: "Build Optimization", description: "Makes a single optimized bundle for your app" },
-    { name: "Powered by SWC", description: "Super speed transpilation via the power of Rust." },
     { name: "Fast & Robust", description: "Built on top of express, for better developer experience." },
+    { name: "Build Optimization", description: "Makes a single optimized bundle for your app" },
     { name: "Easy Setup", description: "Hit `npx blazze-init` to start building your projects." },
     { name: "No Boiler Plate Code", description: "Install init and start, Nothing else 🚀" },
 ]
@@ -20,7 +20,7 @@ export default function Features() {
                 <div className="flex flex-col  items-center justify-center gap-16">
                     <div className="mt-1 flex flex-col gap-3 px-4 text-center sm:px-0">
 
-                        <a className="mx-auto mt-12 mb-4 bg-black text-white dark:bg-white dark:text-black rounded-2xl p-2 shadow-[0_0_2rem_-0.1rem_#fffb]" href="#features">
+                        {/* <a className="mx-auto mt-12 mb-4 bg-black text-white dark:bg-white dark:text-black rounded-2xl p-2 shadow-[0_0_2rem_-0.1rem_#fffb]" href="#features">
                             <div className="group relative overflow-hidden rounded-xl px-3 py-1 duration-300 hover:pr-9 ">
                                 <span className="font-semibold text-md group-hover:font-bold">
                                     <svg
@@ -40,23 +40,22 @@ export default function Features() {
                                     />
                                 </span>
                             </div>
-                        </a>
+                        </a> */}
 
-
-                        <h1 id='features' className='text-3xl sm:text-5xl lg:text-6xl leading-8 my-10 tracking-tighter font-black'>
+                        <h1 id='features' className='text-3xl sm:text-5xl lg:text-6xl leading-8 my-10 tracking-tighter font-[700]'>
                             What's in Blazze ?
                         </h1>
                         <div className='text-neutral-600 font-medium text-md'>All you need to write API's with ease & peace 😌</div>
 
                         <br />
 
-                        <div className='relative z-10 grid w-full gap-4 md:grid-cols-1 lg:grid-cols-2 lg:gap-4'>
+                        <div className='relative z-10 grid w-full gap-4 md:grid-cols-1 lg:grid-cols-2 lg:gap-8'>
                             {
                                 features.map((f, id) => {
                                     return <FeatureCard key={id} className='w-[100%] h-fit hover:shadow-2xl shadow-blue-500'>
                                         <div className='group w-full h-52 md:h-44 p-8 rounded-xl flex 
 											flex-col items-start justify-center text-left gap-2'>
-                                            <h1 className='tracking-tighter font-black text-2xl lg:text-3xl m-4 flex items-center bg-gradient-to-r
+                                            <h1 className='tracking-tighter font-[700] text-2xl lg:text-3xl m-4 flex items-center bg-gradient-to-r
                                             from-blue-500 to-fuchsia-500 bg-clip-text group-hover:text-transparent gap-1'>
                                                 <span className='rounded-full w-10 text-base mx-2 h-10 flex items-center justify-center p-2
                                                 bg-blue-500/40 text-blue-500 group-hover:bg-white group-hover:text-black'>
